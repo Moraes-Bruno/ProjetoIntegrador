d48e77064b2715bb556655c7ef0a7c476fd2d721
Moraes-Bruno <moraesbruno888@gmail.com>
2022-10-18 20:15:28 -0300

    commit

--- Projeto Integrador/html/pdo.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    define('HOST', 'localhost');
-    define('USER', 'root');
-    define('PASS', '');
-    define('BASE', 'portal_de_vagas');
-
-
-    $conn = new MYSQLi(HOST,USER, PASS,BASE);
--- ProjetoIntegrador/html/cadastro.php
+++ ProjetoIntegrador/html/cadastro.php
@@ -4,24 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro</title>
+    <title>Meu Perfil</title>
     <link rel="stylesheet" href="../css/perfilAluno.css">
     <link rel="stylesheet" href="../css/nav.css">
+    <link rel="stylesheet"
+    href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <script src="https://kit.fontawesome.com/20764abc40.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <header>
-        <a href="index.html" class="logo">Fatec<span> Vagas</span></a>
+        <a href="PerfilAluno.php" class="logo">Fatec<span> Vagas</span></a>
         <div class="bx bx-menu" id="menu-icon"></div>
 
         <ul class="navlist">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Home</a></li>
+            <li><a href="vagas.php">Vagas</a></li>
+            <li><a href="#">Minhas vagas</a></li>
         </ul>
 
         <div class="navDireita">
-            <a href="perfilAluno.html"><img src="../imgs/Paula_redonda_icone.png" class="imgPerfil"alt="Imagem perfil"></a>
+            <a href="perfilAluno.php"><img src="../imgs/Paula_redonda_icone.png" class="imgPerfil"alt="Imagem perfil"></a>
                <p>Paula Raquel lima</p>
         </div>
     </header>   
@@ -97,6 +98,14 @@
             </div>
 
         </main>
+        <footer class="footer">
+        <a href="https://www.fatecitapira.edu.br/">Fatec Itapira</a>
+        <div class="imgs-footer">
+
+            <i class="fa-brands fa-square-facebook"></i>
+            <i class="fa-brands fa-instagram"></i>
+        </div>
+    </footer>
     <script type="text/javascript" src="../js/Nav.js"></script>
 </body>
 </html>
